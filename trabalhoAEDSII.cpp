0957bc1413487c8c194c7e5fe7b59c2e72b229dc
GustavoAlot <106634494+GustavoAlot@users.noreply.github.com>
2022-11-01 19:42:31 -0300

    Update trabalhoAEDSII.cpp

--- trabalhoAEDSII.cpp
+++ trabalhoAEDSII.cpp
@@ -1,20 +1,17 @@
-#include <stdio.h>
-#include <iostream>
-#include <fstream>
+#include <bits/stdc++.h>
 
 
 using namespace std;
 
-int TAM = 0;
 
-typedef struct Veiculo
-{
+
+typedef struct {
     string modelo;
     string marca;
     string tipo;
     int ano;
     int km;
-    float potencia_do_motor;
+    float potencia;
     string combustivel;
     string cambio;
     string direcao;
@@ -22,219 +19,175 @@ typedef struct Veiculo
     int portas;
     string placa;
     float valor;
-};
+}Tveiculo;
 
-Veiculo * busca(Veiculo*bd[],int TAM,string placa){
-    int i;
-
-    for(i=0; i<TAM;i++)
-        if(bd[i]->placa == placa)
+Tveiculo * busca (Tveiculo * bd[], int tam,string placa){
+    for (int i = 0; i < tam; i++)
+        if (bd[i] -> placa == placa)
             return bd[i];
-    return NULL;
-    
-}  
+        return NULL;
+}
 
-int buscaindicie(Veiculo *bd[],int TAM,string placa){
-    int i;
-
-    for(i=0; i<TAM;i++)
-        if(bd[i]->placa == placa){
+int buscaindice (Tveiculo * bd[], int tam, string placa){
+    for (int i = 0; i < tam; i++)
+        if (bd[i] -> placa == placa)
             cout << i;
-           
+}
+
+void remove_veiculo(string y, int tam, Tveiculo * bd[]){
+
+    for (int i = 0; i < tam; i++)
+        if (bd[i] -> placa == y){
+            delete bd[i];
+            bd[i] = NULL;
         }
 
-}
-
-
-
-void inserirVeiculo(Veiculo *bd[], int TAM){
-
-    struct {
-    string modelo;
-    string marca;
-    string tipo;
-    int ano;
-    int km;
-    float potencia_do_motor;
-    string combustivel;
-    string cambio;
-    string direcao;
-    string cor;
-    int portas;
-    string placa;
-    float valor;
-} Newveiculo;
-
-    cout << "digite o modelo do veiculo";
-    cin >> Newveiculo.modelo;
-
-    cout << "digite o marca do veiculo";
-    cin >> Newveiculo.marca;
-
-    cout << "digite o tipo do veiculo";
-    cin >> Newveiculo.tipo;
-
-    cout << "digite o ano do veiculo";
-    cin >> Newveiculo.ano;
-
-    cout << "digite o quilometragem do veiculo";
-    cin >> Newveiculo.km;
-
-    cout << "digite a potencia do motor do veiculo";
-    cin >> Newveiculo.potencia_do_motor;
-
-    cout << "digite o combustivel do veiculo";
-    cin >> Newveiculo.combustivel;
-
-    cout << "digite o cambio do veiculo";
-    cin >> Newveiculo.cambio;
-
-    cout << "digite a direcao do veiculo";
-    cin >> Newveiculo.direcao;
-
-    cout << "digite a cor do veiculo";
-    cin >> Newveiculo.cor;
-
-    cout << "digite o numero de portas do veiculo";
-    cin >> Newveiculo.portas;
-
-    cout << "digite a placa do veiculo";
-    cin >> Newveiculo.placa;
-
-    cout << "digite o valor do veiculo";
-    cin >> Newveiculo.valor;
-
-
-
-
+            
 
 
 
 
 
 }
+
+
+
+
+
+float modulo(float a, float b){
+    if (a>b)
+        return a-b;
+    else
+        return b-a;
+}
+
+
+
+int x;
+string y;
+int u;
+
+int main(int argc, char** argv){
     
-
-    
-
-int main(){
-
-    int TAM = 0;
-
-    Veiculo *bd[50];
-    
-
     ifstream myfile ("BD_veiculos_2.txt");
+    Tveiculo * bd[50];
+    Tveiculo * p;
+    p = new Tveiculo;
+    int tam = 0;
+    
+    
     if (myfile.is_open()){
-        while (!myfile.eof()){
-            bd[TAM]= new Veiculo;
-            myfile >> bd[TAM]-> modelo;
-            myfile >> bd[TAM]-> marca;
-            myfile >> bd[TAM]-> tipo;
-            myfile >> bd[TAM]-> ano;
-            myfile >> bd[TAM]-> km;
-            myfile >> bd[TAM]-> potencia_do_motor;
-            myfile >> bd[TAM]-> combustivel;
-            myfile >> bd[TAM]-> cambio;
-            myfile >> bd[TAM]-> direcao;
-            myfile >> bd[TAM]-> cor;
-            myfile >> bd[TAM]-> portas;
-            myfile >> bd[TAM]-> placa;
-            myfile >> bd[TAM]-> valor;
-            TAM++;
-
+        while ( !myfile.eof() ){
+            bd[tam] = new Tveiculo; 
+            myfile >> bd[tam]->modelo;
+            myfile >> bd[tam]->marca;
+            myfile >> bd[tam]->tipo;
+            myfile >> bd[tam]->ano;
+            myfile >> bd[tam]->km;
+            myfile >> bd[tam]->potencia;
+            myfile >> bd[tam]->combustivel;
+            myfile >> bd[tam]->cambio;
+            myfile >> bd[tam]->direcao;
+            myfile >> bd[tam]->cor;
+            myfile >> bd[tam]->portas;
+            myfile >> bd[tam]->placa;
+            myfile >> bd[tam]->valor;
+            tam++;
         }
         myfile.close();
         
+        
+        /*for (int i = 0; i < tam; i++){
+            cout<< bd[i]->modelo << "  ";
+            cout<< bd[i]->marca << "  ";
+            cout<< bd[i]->tipo << "  ";
+            cout<< bd[i]->ano << "  ";
+            cout<< bd[i]->km << "  ";
+            cout<< bd[i]->potencia << "  ";
+            cout<< bd[i]->combustivel << "  ";
+            cout<< bd[i]->cambio << "  ";
+            cout<< bd[i]-> direcao << "  ";
+            cout<< bd[i]->cor << "  ";
+            cout<< bd[i]->portas << "  ";
+            cout<< bd[i]->placa << "  ";
+            cout<< bd[i]->valor << endl;
+        }*/
+        //Apontando os espaços vazios para NULL.
+        for (int i = tam; i < 50; i++)
+            bd[i] = NULL;
+        
+       /* // Removendo as structs da memória.
+        for (int i = 0; i < tam; i++)
+            delete (bd[i]);*/
     }
     else
-        cout << "nao foi possivel abrir arquivo";
-
-
-    for( int i=0; i< TAM; i++){
-            cout << bd[i]-> modelo<<" ";
-            cout << bd[i]-> marca<<" ";
-            cout << bd[i]-> tipo<<" ";
-            cout << bd[i]-> ano<<" ";
-            cout << bd[i]-> km<<" ";
-            cout << bd[i]-> potencia_do_motor <<" ";
-            cout << bd[i]-> combustivel<<" ";
-            cout << bd[i]-> cambio<<" ";
-            cout << bd[i]-> direcao<<" ";
-            cout << bd[i]-> cor<<" ";
-            cout << bd[i]-> portas<<" ";
-            cout << bd[i]-> placa<<" ";
-            cout << bd[i]-> valor<<" "<< endl;
-    }
-
-    for (int i = TAM; i<50; i++){
-        bd[i]= NULL;
-    }
-
+        cout << "Arquivo não encontrado. " << endl;
     
     
+    cout << "[1] Incluir novo veículo." << endl;
+    cout << "[2] Busca por placa." << endl;
+    cout << "[3] Buscar os 10 veículos próximos." << endl;
+    cout << "[4] Ordenação por placa." << endl;
+    cout << "[5] Sair do programa." << endl;
     
-    /*char op[50];
-    int newop;
-    do{
-        cout << "\n\n\n";
-        cout << "\n\t\t    Gerenciador de busca de veiculos\n\n";
-        cout << "\t\t     ( 1 ) Cadastrar novo veiculo\n";
-        cout << "\t\t     ( 2 ) Buscar veiculo por placa\n";
-        cout << "\t\t     ( 3 ) Buscar veiculo por valor\n";
-        cout << "\t\t     ( 4 ) ordenar BD por placas\n";
-        cout << "\t\t     ( 5 ) sair do programa ";
-
-        cout << "\nOpcao Desejada: ";
-        scanf("%s", &op);
-        newop = atoi(op) ;     
-
-        switch (newop){
+    cout<< "Insira a opção desejada:"<< endl;
+    cin>> x;
+    
+    
+    switch(x){
+            case 1:
+                
+                break;
+                
+            case 2:
+                cout<< "Insira a placa:" << endl;
+                cin >> y;
+                p = busca (bd, tam, y);
+                if (p){
+                    cout << "Veículo encontrado:" << endl;
+                    cout << p->marca << " " << p->ano << " " << endl;
+                    cout << "Deseja remover o veículo? [1]=sim ou [2]=não" << endl;
+                    cin >> u;
             
-        case 1:
-            cout << "voce desejou cadastrar um veiculo novo";
-
-            break;
-            
-        case 2:
-            cout << "voce desejou buscar um veiculo por placa";
-            break;
-        
-        case 3:
-            cout << "voce desejou buscar um veiculo por valor";
-            break;
-        
-        case 4:
-            cout << "voce desejou ordenar o BD por placa";
-            break;
-        
-        case 5:
-            cout << "voce desejou sair do programa ;-; \n\n";
-            return 0;
-            break;
-
+            switch(u){
+                case 1:
+                    cout << "Veículo removido"<< endl;
+                    break;
+                    
+                case 2:
+                    cout << "Veículo não removido" << endl;
+                    break;
+                    
+                default:
+                    cout << "Opção inválida"<< endl;
+            }
+        }
+        else {
+            cout << "Veículo não encontrado";
+        }
+                break;
+                
+            case 3:
+                cout << "[3] Buscar os 10 veículos próximos.";
+                break;
+                
+            case 4:
+                cout << "[4] Ordenação por placa.";
+                break;
+                
+            case 5:
+                cout << "[5] Sair do programa.";
+                break;
         default:
-            printf("\n\t\tOpção não válida\n\n");
-        } 
-    } while (op !=0);
-
-    system("pause");
-    return 0 ; */
-
-
-    cout << "\n\n o tamanho é" ;
-}
-
-
-
-
-
-
-
-
+            cout << "Opção inválida.";
+                
+        }
     
-
-        
-
-
-
+  
+    
+    
+    
+    
+    
+    return 0;
+}
